67e3cef9bbe14d46460e38bec40523cd5ad1f7df
128na <simutrans.128na@gmail.com>
2020-02-08 11:37:38 +0900

    add description

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # simutrans-vscode-extention
+This extension provides syntax highlighting and snippets.
+
+## syntax highlighting
+
+![syntax highlighting](./docs/syntax-highlighting.png)
+
+## snippets
+
+![snippets](./docs/snippets.png)
---
+++ src/extension.js
@@ -0,0 +1,16 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+  activate,
+  deactivate
+}
